fc35e892b57ac4e43699ecf206ffb5a407e30527
abhideveloping <45909234+abhideveloping@users.noreply.github.com>
2021-08-27 18:40:14 +0530

    updated

---
+++ script.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector('button');
+let screenSize = document.querySelector('.screenSize');
+let windowSize = document.querySelector('.windowSize');
+let contentSize = document.querySelector('.contentSize');
+
+btn.addEventListener('click', () => {
+    screenSize.innerText = `Screen Height: ${screen.height} - Screen Width: ${screen.width}`;
+
+    windowSize.innerText = `Window Height: ${window.outerHeight} - Window Width: ${window.outerWidth}`;
+
+    contentSize.innerText = `Content Height: ${window.innerHeight} - Content Width: ${window.innerWidth}`;
+});
